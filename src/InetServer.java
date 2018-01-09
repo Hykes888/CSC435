e40e222b3fb7c6970750724fa3751d93981f16a7
Hykes888 <Hykes888@gmail.com>
2018-01-08 21:24:28 -0600

    First Commit need to add commits

---
+++ src/InetServer.java
@@ -0,0 +1,70 @@
+/
+import java.io.*;
+import java.net.*;
+
+class Worker extends Thread {
+    Socket sock;
+
+    Worker(Socket s) {
+        sock = s;
+    }
+
+    public void run() {
+        PrintStream out = null;
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            out = new PrintStream(sock.getOutputStream());
+            try {
+                String name;
+                name = in.readLine();
+                System.out.println("Looking up " + name);
+                printRemoteAddress(name, out);
+            } catch (IOException x) {
+                System.out.println("Server read error");
+                x.printStackTrace();
+            }
+            sock.close();
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+        }
+    }
+
+    static void printRemoteAddress(String name, PrintStream out) {
+        try {
+            out.println("Looking up " + name + "...");
+            InetAddress machine = InetAddress.getByName(name);
+            out.println("Host name : " + machine.getHostName());
+            out.println("Host IP : " + toText(machine.getAddress()));
+        } catch (UnknownHostException ex) {
+            out.println("Failed in attempt to look up " + name);
+        }
+    }
+
+    static String toText(byte ip[]) {
+        StringBuffer result = new StringBuffer();
+        for (int i = 0; i < ip.length; ++i) {
+            if (i > 0) result.append(".");
+            result.append(0xff & ip[i]);
+        }
+        return result.toString();
+    }
+}
+
+
+    public class InetServer {
+        public static void main(String a[]) throws IOException {
+            int q_len = 6;
+            int port = 1565;
+            Socket sock;
+
+            ServerSocket servsock = new ServerSocket(port, q_len);
+
+            System.out.println("Clark Elliott's Inet server 1.8 starting up, listening to port 1565.\n");
+            while (true) {
+                sock = servsock.accept();
+                new Worker(sock).start();
+            }
+        }
+    }
+

5f9a653a99001d07ca22f10dda1f9dff7f0d7fdc
Hykes888 <Hykes888@gmail.com>
2018-01-08 22:21:34 -0600

    Added comments

--- src/InetClient.java
+++ src/InetClient.java
@@ -1,57 +1,70 @@
-import java.io.*;
-import java.net.*;
+import java.io.*; //Get the Input Output libraries
+import java.net.*; //Get the Java Networking libraries
 
+/**
+ * This is the client code and it sends requests to the server to be able to do work.
+ */
 public class InetClient {
 
+    //If there is an input of IP address it will use that IP address of server or it assumes its just localhost.
     public static void main(String[] args) {
-        String serverName;
+        String serverName; //variable
+        //If then else statement that again says if I start the client with a server IP address I will be able to use the IP address otherwise its local host as the IP address.
         if(args.length < 1) serverName = "localhost";
         else serverName = args[0];
 
-        System.out.println("Clark Elliott's Inet Client, 1.8.\n");
-        System.out.println("Using Server: " + serverName + ", Port:1565");
+        System.out.println("Tim Heichele Inet Client, 1.8.\n");
+        System.out.println("Using Server: " + serverName + ", Port:1865");
+        //Create a new input to the server which it reads input coming from the console.
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         try{
-            String name;
+            String name; //Local variable.
+            //A while loop that is forever until you enter quit. But the other function is to enter an IP address or hostname for a the server to look up.
             do{
                 System.out.print("Enter a hostname or an IP address, (quit) to end: ");
                 System.out.flush();
                 name = in.readLine();
                 if (name.indexOf("quit") < 0)
+                    // Calls method getRemoteAddress, 2 args string and serverName
                     getRemoteAddress(name, serverName);
             } while (name.indexOf("quit") < 0);
             System.out.println("Cancelled by user request.");
-        } catch (IOException x) {x.printStackTrace();}
+        } catch (IOException x) {x.printStackTrace();} //error handling
     }
 
+    //Method that takes an IP address in bytes and formats it into text. Args bytes in an array
     static String toText (byte ip[]) {
-        StringBuffer result = new StringBuffer();
+        StringBuffer result = new StringBuffer();// crate a string buffer for the ip address formatting
+        //Java for loop that breaks down the IP address into readable text for the output to the sever console.
         for (int i=0; i < ip.length; ++i){
             if (i > 0) result.append(".");
             result.append(0xff & ip[i]);
         }
-        return result.toString();
+        return result.toString(); // returns a string
     }
 
+    //This method sends the request to the server with two inputs the name and the serverName.
     static void getRemoteAddress(String name, String serverName) {
+        //Create local variables
         Socket sock;
         BufferedReader fromServer;
         PrintStream toServer;
         String textFromServer;
 
         try{
-            sock = new Socket(serverName,1565);
-            fromServer = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-            toServer = new PrintStream(sock.getOutputStream());
-            toServer.println(name);
-            toServer.flush();
+            sock = new Socket(serverName,1865); //Create connection
+            fromServer = new BufferedReader(new InputStreamReader(sock.getInputStream())); //input from the server
+            toServer = new PrintStream(sock.getOutputStream()); //sending to the server
+            toServer.println(name); // print line on console with local name or requested work
+            toServer.flush(); //New line
+            //This for is outputting the data it is recieving from the server after it did its work.
             for(int i = 1; i <= 3; i++){
                 textFromServer = fromServer.readLine();
                 if (textFromServer != null)
                     System.out.println(textFromServer);
             }
-            sock.close();
-        } catch (IOException x){
+            sock.close(); // close socket or connection
+        } catch (IOException x){ //error handling
             System.out.println("Socket error.");
             x.printStackTrace();
         }
--- src/InetServer.java
+++ src/InetServer.java
@@ -1,66 +1,81 @@
-/
-import java.io.*;
-import java.net.*;
+import java.io.*; //Get the Input Output libraries
+import java.net.*; //Get the Java Networking libraries
 
-class Worker extends Thread {
-    Socket sock;
-
+/**
+ * File: InetServer.java
+ *
+ * A simple client server java app.
+ *
+ * The code below is the server side of the java app. It performs all requests sent to in.
+ *
+ */
+class Worker extends Thread { //Class definition.
+    Socket sock; //Socket create the connection.
+    //Constrctor, args Socket s assigns to local sock.
     Worker(Socket s) {
         sock = s;
     }
-
+    //When you extend thread you have to run the thread and each time a request comes into the server a new thread is created.
     public void run() {
+        // These two are the input and output of the socket connection.
         PrintStream out = null;
         BufferedReader in = null;
+        //Try/catch allow for error handling.
         try {
+            //Create a new input so we are getting the input from the client. This is setup.
             in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            //Create a new output so we are able to send the output to the client. This is setup
             out = new PrintStream(sock.getOutputStream());
             try {
-                String name;
-                name = in.readLine();
-                System.out.println("Looking up " + name);
-                printRemoteAddress(name, out);
-            } catch (IOException x) {
+                String name; //variable
+                name = in.readLine(); // read from the input queue
+                System.out.println("Looking up " + name); // Print the out put from the what was sent from the client.
+                printRemoteAddress(name, out); //Calls method printRemoteAddress with its two arguments.
+            } catch (IOException x) { // error handling.
                 System.out.println("Server read error");
                 x.printStackTrace();
             }
-            sock.close();
-        } catch (IOException ioe) {
+            sock.close(); //close the connection after the work has been finished.
+        } catch (IOException ioe) { // error handling
             System.out.println(ioe);
         }
     }
-
+    //This method does a DNS lookup using a java library. Two args are need for this method a String and an output stream.
     static void printRemoteAddress(String name, PrintStream out) {
         try {
-            out.println("Looking up " + name + "...");
-            InetAddress machine = InetAddress.getByName(name);
-            out.println("Host name : " + machine.getHostName());
-            out.println("Host IP : " + toText(machine.getAddress()));
-        } catch (UnknownHostException ex) {
+            out.println("Looking up " + name + "..."); //Prints line on clients console
+            InetAddress machine = InetAddress.getByName(name); // java DNS lookup
+            out.println("Host name : " + machine.getHostName()); // Prints line on clients console which is the host name.
+            out.println("Host IP : " + toText(machine.getAddress())); // Call method toTest, Prints lines on clients console which will be the IP address of the URL lookup.
+        } catch (UnknownHostException ex) { //error handling
             out.println("Failed in attempt to look up " + name);
         }
     }
-
+    //Method that takes an IP address in bytes and formats it into text. Args bytes in an array
     static String toText(byte ip[]) {
-        StringBuffer result = new StringBuffer();
+        StringBuffer result = new StringBuffer(); // crate a string buffer for the ip address formatting
+        //Java for loop that breaks down the IP address into readable text for the output to the client console.
         for (int i = 0; i < ip.length; ++i) {
             if (i > 0) result.append(".");
             result.append(0xff & ip[i]);
         }
-        return result.toString();
+        return result.toString(); // returns a string.
     }
 }
 
-
-    public class InetServer {
+/**
+ * This Class has the main for the server and starts a forever loop that will accept client request and start communication with them using a socket.
+ */
+public class InetServer {
         public static void main(String a[]) throws IOException {
             int q_len = 6;
-            int port = 1565;
-            Socket sock;
+            int port = 1865; // Connection port 1865
+            Socket sock; // variable of a java socket.
 
-            ServerSocket servsock = new ServerSocket(port, q_len);
+            ServerSocket servsock = new ServerSocket(port, q_len); // crate new socket for the server.
 
-            System.out.println("Clark Elliott's Inet server 1.8 starting up, listening to port 1565.\n");
+            System.out.println("Tim Heichele Inet server 1.8 starting up, listening to port 1865.\n");
+            //Forever loop that will will accept all requests and then create a new thread and perform work for that client.
             while (true) {
                 sock = servsock.accept();
                 new Worker(sock).start();
